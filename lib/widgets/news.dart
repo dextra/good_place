c09d5d4d3fa5aa4a7d0fbd71e6f1d630724e5b9d
Tyemy Kuga <tyemy.kuga@dextra-sw.com>
2021-06-11 23:30:50 -0300

    News layout

--- lib/widgets/news.dart
+++ lib/widgets/news.dart
@@ -24,57 +24,66 @@ class News extends StatelessWidget {
               Assets.news,
               width: 70,
             ),
-            SizedBox(
-              width: 15,
-            ),
+            SizedBox(width: 15),
             Label(
               text: title ?? 'Notícias',
               labelType: LabelType.littleTitle,
             ),
           ],
         ),
-        ...items.map(
-          (item) => Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 15, bottom: 15),
-                child: Container(
-                  child: Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Label(
-                              text: item.title,
-                              labelType: LabelType.cardTitle,
+        SizedBox(height: 15),
+        Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              color: CustomColors.light_green),
+          child: Column(
+            children: items
+                .map(
+                  (item) => Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10, bottom: 10),
+                        child: Container(
+                          child: Padding(
+                            padding: const EdgeInsets.all(20),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Label(
+                                      text: item.title,
+                                      labelType: LabelType.cardTitle,
+                                    ),
+                                    Label(
+                                      text: format.format(item.date),
+                                      labelType: LabelType.cardDate,
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(height: 10),
+                                Label(
+                                  text: item.content,
+                                  labelType: LabelType.cardBody,
+                                ),
+                              ],
                             ),
-                            Label(
-                              text: format.format(item.date),
-                              labelType: LabelType.cardDate,
-                            ),
-                          ],
+                          ),
                         ),
-                        SizedBox(height: 10),
-                        Label(
-                          text: item.content,
-                          labelType: LabelType.cardBody,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 20, left: 20),
+                        child: Container(
+                          height: 1,
+                          color: CustomColors.medium_gray,
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      color: CustomColors.light_green),
-                ),
-              ),
-              Container(
-                height: 1,
-                color: CustomColors.medium_gray,
-              ),
-            ],
+                )
+                .toList(),
           ),
         ),
       ],
